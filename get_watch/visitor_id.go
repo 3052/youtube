d4017f54ee5925293966ddf89e55781448211fbb
3052 <spen+3052@protonmail.com>
2024-09-24 05:54:00 -0500

    modified:   get_watch/visitor_id.go

--- get_watch/visitor_id.go
+++ get_watch/visitor_id.go
@@ -1,37 +1,37 @@
 package youtube
 
 import (
-	"154.pages.dev/protobuf"
-	"bytes"
-	"io"
-	"net/http"
+   "154.pages.dev/protobuf"
+   "bytes"
+   "io"
+   "net/http"
 )
 
 func visitor_id() (protobuf.Message, error) {
-	message := protobuf.Message{
-		1: {protobuf.Message{
-			1: {protobuf.Message{
-				16: {protobuf.Varint(3)},
-				17: {protobuf.Bytes("19.33.35")},
-			}},
-		}},
-	}
-	resp, err := http.Post(
-		"https://youtubei.googleapis.com/youtubei/v1/visitor_id",
-		"application/x-protobuf", bytes.NewReader(message.Marshal()),
-	)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	message = protobuf.Message{}
-	err = message.Unmarshal(data)
-	if err != nil {
-		return nil, err
-	}
-	return message, nil
+   message := protobuf.Message{
+      1: {protobuf.Message{
+         1: {protobuf.Message{
+            16: {protobuf.Varint(3)},
+            17: {protobuf.Bytes("19.33.35")},
+         }},
+      }},
+   }
+   resp, err := http.Post(
+      "https://youtubei.googleapis.com/youtubei/v1/visitor_id",
+      "application/x-protobuf", bytes.NewReader(message.Marshal()),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   data, err := io.ReadAll(resp.Body)
+   if err != nil {
+      return nil, err
+   }
+   message = protobuf.Message{}
+   err = message.Unmarshal(data)
+   if err != nil {
+      return nil, err
+   }
+   return message, nil
 }
